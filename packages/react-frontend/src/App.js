9fd96fea58c0c4f6a81aa8a93a0ced12ffe29608
Nick Bircher <nicky.b.three@gmail.com>
2023-10-09 18:10:02 -0700

    Add useEffect to fetchUsers upon mounting

--- packages/react-frontend/src/App.js
+++ packages/react-frontend/src/App.js
@@ -1,9 +1,16 @@
-import React, {useState} from "react";
+import React, { useState, useEffect } from "react";
 import Table from "./Table";
 import Form from './Form';
 
 function MyApp() {
   const [characters, setCharacters] = useState([]);
+
+  useEffect(() => {
+	  fetchUsers()
+		  .then((res) => res.json())
+	    .then((json) => setCharacters(json["users_list"]))
+	    .catch((error) => { console.log(error); });
+	}, [] );
   
   function removeOneCharacter (index) {
     const updated = characters.filter((character, i) => {
